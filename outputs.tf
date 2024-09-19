abfd7caff099eb2d2d506c3ef808012cac0fb9be
Olivier Hoareau <oha@greenberets.io>
2024-09-19 18:06:13 +0200

    fix: fix

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,8 @@ output "user_pools" {
     users = {
       arn = aws_cognito_user_pool.main.arn
       name = aws_cognito_user_pool.main.name
-      endpoint = aws_cognito_user_pool.main.endpoint
+      endpoint = "https://${var.dns}"
+      internal_endpoint = aws_cognito_user_pool.main.endpoint
       id = aws_cognito_user_pool.main.id
       clients = {for k,v in aws_cognito_user_pool_client.client: k => {name = v.name, id = v.id}}
     }
